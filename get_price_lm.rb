f80d72be51b07f40e2337670ba973f46bf85abfd
Sproodigy <sproodigy@gmail.com>
2017-08-13 01:16:28 +0400

    Added ability to write only new lines

--- get_price_lm.rb
+++ get_price_lm.rb
@@ -28,23 +28,23 @@
   z = []
 
   def get_lm_product_data(product_id)
-    # Load page html and parse it with Nokogiri
+# Load page html and parse it with Nokogiri
     page = Nokogiri::HTML(HTTP.follow.get("http://www.lm-shop.ru/index.php?route=product/product&product_id=#{product_id}").to_s)
-    # Select html element from page and print it
+# Select html element from page and print it
     [
-      # Art (0)
+# Art (0)
       page.css('#tab-attribute > table.attribute > tbody > tr:nth-last-child(2) > td:nth-child(2)').first&.content,
-      # Full description (1)
+# Full description (1)
       page.css('.product-description').first&.content,
-      # Name, short decription and volume (2)
+# Name, short decription and volume (2)
       (page.css('.product-info h1').first&.content unless page.css('.product-info h1').first&.content.nil?),
-      # New price (3)
+# New price (3)
       page.css('.product-info span.price-new').first&.content&.gsub(/[^0-9\.]/, '')&.to_f,
-      # Image (4)
+# Image (4)
       (page.css('a#zoom_link1').first[:href] unless page.css('a#zoom_link1').first.nil?),
-      # Weight
+# Weight
       page.css('#tab-attribute > table.attribute > tbody > tr:nth-last-child(1) > td:nth-child(2)').first&.content,
-      # Old price (6)
+# Old price (6)
       page.css('.product-info .price').first&.content&.gsub(/[^0-9\.]/, '')&.to_f,
 
       page.css('.infoleft')
@@ -53,7 +53,7 @@
   end
 
   def get_purchase_price(product_art)
-    # Also supports csv, csvt and tsv formats
+# Also supports csv, csvt and tsv formats
 
     s = SimpleSpreadsheet::Workbook.read('app/assets/prices/Price_LM_02.08.2017.xlsx')
     s.selected_sheet = s.sheets[0]
@@ -86,12 +86,6 @@
       end
     end
   end
-
-  # Read CSV files
-  # CSV.open('test.csv', 'r') do |row|
-  #   p row
-  # end
-
 
   def get_lm_product_image(product_id)
 
@@ -141,19 +135,14 @@
   #                                        weight: weight
   #                                       }})
   # end
+
   src_for_csv = []
 
-  (0..7).each do |product_id|
+  (0..30).each do |product_id|
 
     result = get_lm_product_data(product_id)
     next if result[0].nil?
 
-    # if result[5] == /[a-zA-Zа-яА-Я]/
-    #   weight = result[5].gsub(/[a-zA-Zа-яА-Я]/, '')
-    #   next if weight > 20
-    # else
-    #   ''
-    # end
     art = result[0]
 
     barcode = barcode_from_product_art(result[0])
@@ -166,7 +155,7 @@
     weight = result[5][0..-2].gsub(/[a-zA-Zа-яА-Я ]/, '') if /[a-zA-Zа-яА-Я]/.match(result[5])
     next if weight.to_f > 20
 
-    store_id = 3 # Avto-Raketa
+    store_id = 3   # Avto-Raketa
 
     if result[2].include?(' — ')
       data = result[2].partition(' — ')
@@ -237,8 +226,6 @@
       sku_full
     end
 
-    # z << ["weight: #{weight}", "purch_price: #{purchase_price}", "price: #{price}", "#{short_desc}:  #{short_desc.length}", title, "#{sku_full}:  #{sku_full.length}", '- - - - - - - - - - - - - -']
-
     case
     when purchase_price.to_f <= 40
       puts "purchase_price too small   #{art}"
@@ -260,8 +247,6 @@
 
     src_for_csv << ["#{art}", "#{title}", "#{short_desc}", "#{sku_full}", "#{barcode}", "#{purchase_price}", "#{price}", "#{weight}"]
 
-    # z << ["#{sku_full}:  #{sku_full.length}", '- - - - - - - - - - - - - -']
-
     # puts get_lm_product_data(product_id)[5]
 
     # puts "#{short_desc}==#{short_desc.length}:   #{result[0]}", title, "#{sku_full}:  " + "#{sku_full.length}", '----------------'
@@ -270,48 +255,33 @@
   #   # puts purchase_price, sku, price, '= - = - ='
   #   # puts put_lm_product_price(purchase_price, barcode, store_id, price, short_desc, title, weight)
   #   # create_product(purchase_price, sku, barcode, store_id, price, short_desc, title, weight)
-  end
-  #
-  # header = ["Art", "Title", "Short description", "SKU", "Barcode", "Purchase price", "Price", "Weight"]
-  # CSV.open('test.csv', 'w', { encoding: "UTF-8", col_sep: ';', headers: true }) do |csv|
-  #   csv << header
-  #   src_for_csv.each do |row|
-  #     csv << row
-  #     puts csv.inspect
-  #   end
-  # end
-  #
-  # m = CSV.read('test.csv', headers: true)
-  #   m.each do |row|
-  #     puts row[1], '= = = = = ='
-  #     puts row.to_a.inspect, '- - - - - - -'
-  #   end
 
-  m = CSV.foreach('test.csv', col_sep: ';', headers:true) do |row|
-      p row.inspect, '- - - - - - -'
-      p row, '= = = = = = = ='
   end
 
-#
-  # CSV.open('test.csv', 'w') do |csv|
-  #   m.each do |row|
-  #     csv << row
-  #   end
+# To add a header, the columns should be written monotonously with the header (each data column separately).
+
+# Save file from data
+#   header = ["Art", "Title", "Short description", "SKU", "Barcode", "Purchase price", "Price", "Weight"]
+#   CSV.open('test.csv', 'w', { encoding: "UTF-8", col_sep: ';', headers: true }) do |csv|
+#     csv << header
+#     src_for_csv.each do |row|
+#       csv << row
+#     end
+#   end
+
+# Read file as individual rows, can translate it into an array (with a header representation) and string (without header representation).
+m = CSV.read('test.csv', col_sep: ';', headers: true)  # Same as CSV.parse(File.read('test.csv'))
+src_for_csv.each do |row|
+  m << row unless m['Art'].include?(row[0])
+end
+puts m
+    # m.delete('Name')   # Remove the column
+    # m.delete(0)   # Remove a row
+
+# Read the file as individual columns.
+  # m = CSV.foreach('test.csv', col_sep: ';', headers:true) do |col|   # Same as CSV.parse('test.csv') { |row| puts row.inspect}
+      # p col.inspect, '- - - - - - -'
+      # p col, '= = = = = = = ='
+      # puts col['Price'], '+ + + + + + +'
+      # puts col[1], '+ + + + + + +'
   # end
-  # puts v.inspect
-
-  # CSV.foreach('test.csv') do |row|
-  #   puts row.inspect
-  # end
-
-  # a_string = "Dan,34\nMaria,55"
-  # CSV.parse(a_string) { |row| puts row.inspect}
-
-  #
-  # CSV.foreach('test.csv', encoding: "UTF-8", headers: true) do |row|   # Same as CSV.parse(a_string) { |row| puts row.inspect}
-  #   puts row
-  # end
-
-# Разбивает файл на отдельные строки-массивы.
-  # a = CSV.parse(File.read('test.csv'))  # Same as CSV.read('test.csv')
-  # puts a[2]
---
+++ read_csv.rb
@@ -0,0 +1,36 @@
+require 'csv'
+
+# Read file as individual rows, can translate it into an array (with a header representation) and string (without header representation).
+  # m = CSV.read('test.csv', headers: true).to_a   # Same as CSV.parse(File.read('test.csv'))
+  # puts m.inspect, m.class, '- - - - - - - -'
+  # m.each do |arr|
+  #   puts arr.to_s[0..6].gsub(/[^0-9]/, '').inspect, arr.to_s.class, ' = = = = = = = = = '
+  # end
+
+  m = CSV.read('test.csv',col_sep: ';', headers: true)  # Same as CSV.parse(File.read('test.csv'))
+  puts m['Art']
+
+  # puts m
+  # src_for_csv.each do |row|   # Array
+  #     if row[0] == m[1][0][0..4].gsub(/[^0-9]/, '')
+  #       next
+  #     else
+  #       m << row
+  #     end
+  # end
+    # m.delete('Name')   # Remove the column
+    # m.delete(0)   # Remove a row
+    # m.each do |row|
+    #   puts row, '= = = = = ='
+    #   puts row.to_a.inspect, '- - - - - - -'
+    #   puts row.to_s.inspect, '+ + + + + + + +'
+    # end
+
+# Read the file as individual columns.
+  # m = CSV.foreach('test.csv', col_sep: ';', headers:true) do |col|   # Same as CSV.parse('test.csv') { |row| puts row.inspect}
+  #   puts col[0]
+      # p col.inspect, '- - - - - - -'
+      # p col, '= = = = = = = ='
+      # puts col['Price'], '+ + + + + + +'
+      # puts col[1], '+ + + + + + +'
+  # end
