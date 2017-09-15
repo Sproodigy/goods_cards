1da8d8c3e72863cd9350bf5baedd47666ea12323
Sproodigy <sproodigy@gmail.com>
2017-09-16 02:49:37 +0400

    changed the site to retrieve data

--- get_lm_price.rb
+++ get_lm_price.rb
@@ -38,6 +38,27 @@
     ]
   end
 
+  def get_lm_product_data_liquimoly_ru(product_id)
+  # Load page html and parse it with Nokogiri
+    page = Nokogiri::HTML(HTTP.follow.get("http://liquimoly.ru/item/#{product_id}.html").to_s)
+  # Select html element from page and print it
+    [
+  # Art (0)
+    page.css('.card_desc strong').first&.content,
+  # Full description
+    # Properties (1)
+    page.css('#tabs-1 p').first,
+    # Application of goods (2)
+    page.css('#tabs-2 p').last,
+  # Name, short decription (3)
+      page.css('.fl_f_div h1').first&.content,
+  # Image (4)
+      (page.css('.fl_f_div a.big_img_l.loupe_target').first[:href] unless page.css('.fl_f_div a.big_img_l.loupe_target').first.nil?),
+  # Weight (5)
+      page.css('.card_desc a').first&.content
+    ]
+  end
+
   def get_purchase_price(product_art)
   # Also supports csv, csvt and tsv formats
     s = SimpleSpreadsheet::Workbook.read('app/assets/prices/Price_LM_02.08.2017.xlsx')
@@ -47,8 +68,10 @@
       pur_price = s.cell(line, 8)
       data_1 = Hash.new
       data_2 = Hash.new
+      data_3 = Hash.new
 
       next if art_shen == nil
+      next if /[^0-9\/*]/.match(art_shen.to_s)
 
       if art_shen.to_s.include?('/')
         double_art = art_shen.to_s.gsub(/[\/]/, ' ').partition(' ')
@@ -58,14 +81,13 @@
           data_1[art_shu] = pur_price
         end
       elsif art_shen.to_s.include?('*')
-        next
+        data_3[art_shen] = pur_price
       else
         art_shu = art_shen.to_s.gsub(/[^0-9]/, '')
         data_2[art_shu] = pur_price
       end
 
-      data_full = data_2.merge!(data_1)
-
+      data_full = data_1.merge!(data_2).merge!(data_3)
       data_full.each do |key, value|
         if key == product_art then value = pur_price
           return pur_price.to_f
@@ -76,18 +98,18 @@
 
   def get_lm_product_image(product_id)
 
-    src = get_lm_product_data(product_id)[4]
-    name = get_lm_product_data(product_id)[2]
+    src = 'http://liquimoly.ru/' + get_lm_product_data_liquimoly_ru(product_id)[4]
+    # name = get_lm_product_data(product_id)[2]
 
     @content_type_data = File.extname(src)
     content_type = 'image/' + @content_type_data[1,3]
     image_base64_data = Base64.encode64(open(src) { |f| f.read })
     @image = "data:#{content_type};base64,#{image_base64_data}"
 # Save file in project foldet
-    # File.open('Liqui_Moly_' + "#{name}" + '_' + File.extname(src)[1, 3], 'wb') { |f| f.write(open(src).read) }
+    # File.open(@title + '.' + File.extname(src)[1, 3], 'wb') { |f| f.write(open(src).read) }
     # Save image to folder on the hard drive
     # IO.copy_stream(open(src), "/home/sproodigy/Foto/Liqui_Moly_#{name}_#{get_lm_product_data(product_id)[1].first(-1)}_#{File.basename(src)}")
-    IO.copy_stream(open(src), "/Users/extra/Documents/Авторакета/LiquiMoly/Фото Liqui Moly/#{@title.gsub(/[\/ ]/, '_') + @content_type_data}")
+    # IO.copy_stream(open(src), "/Users/extra/Documents/Авторакета/LiquiMoly/Фото Liqui Moly/#{@title.gsub(/[\/ ]/, '_') + @content_type_data}")
   end
 
 
@@ -167,38 +189,57 @@
                                         }})
   end
 
-
-
-
   # @src_for_csv = []
   art_count = []
 
-  (514..516).each do |product_id|   # Art from 0 to 2357
+  (1007..1007).each do |product_id|   # Art from 1007 to 77169
 
-    result = get_lm_product_data(product_id)
-    next if result[0].nil?
-
-    art = result[0]
+    end_date = get_lm_product_data_liquimoly_ru(product_id)
+    next if end_date[0].nil?
+    art = end_date[0].rpartition(' ').last
     art_count << art
 
-    barcode = barcode_from_product_art(result[0])
+    weight = end_date[5].split(' ')[-2]
+
+    if end_date[5].include?('шт')
+      weight = 0.1
+    end
+
+    short_desc = end_date[3].gsub(/[^А-Яа-я]/, ' ').rstrip.lstrip + '.'
+
+    if short_desc.length > 64   # For Extrastore
+      data = short_desc[0..63].split(' ')
+      data.pop
+      short_desc = data.join(' ').rstrip + '.'
+    end
+
+    if short_desc.include?('масло')   # For Extrastore
+      category_ids = [1206]
+    elsif short_desc.include?('салфетки')
+      category_ids = [1277]
+    else
+      category_ids = [1201]
+    end
+
+    @title ='Liqui Moly ' + end_date[3].gsub(/[А-Яа-я]/, ' ').lstrip.rstrip + " (#{weight} кг)" + " (art: #{art})"
+
+    get_lm_product_image(product_id)
+    filename = @title.gsub(/ /, '_') + @content_type_data
+    image = @image
+
+    title = @title
+
+    full_desc = '<p><h3>Свойства</h3></p>' + end_date[1] + '<p><h3>Применение</h3></p>' + end_date[2]   # For Extrastore
+
+    barcode = barcode_from_product_art(art)
 
     sku = barcode
 
-    price = result[3]
-    old_price = result[6]
-    if result[3] == nil then old_price end
+    purch_price = get_purchase_price(art)   # For Extrapost
 
-    purchase_price = get_purchase_price(result[0])   # For Extrapost
+    price = (purch_price * 1.356).round
 
-    weight = result[5][0..-2].gsub(/[a-zA-Zа-яА-Я ]/, '') if /[a-zA-Zа-яА-Я]/.match(result[5])   # For Extrapost
-    if weight.nil?
-      weight = 0.1
-    else
-      weight = weight
-    end
-
-    full_desc = result[1]   # For Extrastore
+    old_price = (purch_price * 1.531).round   # For Extrastore
 
     country_of_origin = 'DE'  # For Extrapost
 
@@ -208,42 +249,97 @@
 
     availability = 'on_demand'   # For Extrastore
 
-    if result[2].include?(' — ')
-      data = result[2].partition(' — ')
-      short_desc = data.last.split
-      short_desc[-2, 2] = nil
-      short_desc = short_desc.compact!.join(' ') + '.'
-      # sku_full = "lm_#{data.first.downcase.gsub(/-|[ ]/, '_')}_#{weight}"
-      @title = "Liqui Moly #{data.first} (#{weight} kg) (art: #{art})"
-    elsif
-      result[2].include?(' - ')
-      data = result[2].partition(' - ')
-      short_desc = data.last.split
-      short_desc[-2, 2] = nil
-      short_desc = short_desc.compact!.join(' ') + '.'
-      @title = "Liqui Moly #{data.first} (#{weight} kg) (art: #{art})"
-      # sku_full = "lm_#{data.first.downcase.gsub(/-|[ ]/, '_')}_#{weight}"
-    else
-      short_desc = ''
-      title_src = ''
-      data = result[2].split(' ') unless result[2].nil?
-      data.each do |word|
-        if /[А-Яа-я]/.match(word)
-          short_desc = short_desc + word + ' '
-        else
-          /[a-zA-Z]/.match(word)
-          title_src = title_src + word + ' '
-        end
-        # sku_full = "lm_#{title_src.downcase.gsub(/-|[ ]/, '_')}_#{weight}"
-        @title = "Liqui Moly #{title_src} (#{weight} kg) (art: #{art})"
-      end
-    end
+    # puts weight, short_desc, title, filename, barcode, purch_price, price, old_price
 
-    if short_desc.length > 64
-      data = short_desc[0..63].split(' ')
-      data.pop
-      short_desc = data.join(' ')
+    case
+    when /[^0-9]/.match(barcode)
+      puts "barcode is NAN   #{art}"
+    when barcode.length > 13
+      puts "barcode too big   #{art}"
+    when art.include?('*')
+      puts "product is not available for order   #{art}"
+    when purch_price.to_f <= 30
+      puts "purchase_price too small   #{art}"
+    when price.to_f <= 30
+      puts "price too small   #{art}"
+    when price.to_f < purch_price.to_f
+      puts "purchase_price too big   #{art}"
+    when /[^0-9.,]/.match(purch_price.to_s)
+      puts "purchase_price is NAN   #{art}"
+    when /[^0-9.,]/.match(price.to_s)
+      puts "price is NAN   #{art}"
+    when /[^0-9.,]/.match(weight.to_s)
+      puts weight.to_s + " weight is NAN   #{art}"
+    when short_desc.length > 64
+      puts "short_desc length > 64    #{art}"
     end
+  end
+  puts 'Number of goods:   ' + "#{art_count.size}", '- - - - -'
+
+    # result = get_lm_product_data(product_id)
+    # next if result[0].nil?
+    #
+    # get_purchase_price(result[0])
+    #
+    # art = result[0]
+    # art_count << art
+    #
+    # barcode = barcode_from_product_art(result[0])
+    #
+    # sku = barcode
+    #
+    # price = result[3]
+    # old_price = result[6]
+    # if result[3] == nil then old_price end
+    #
+    # purchase_price = get_purchase_price(result[0])   # For Extrapost
+    #
+    # weight = result[5][0..-2].gsub(/[a-zA-Zа-яА-Я ]/, '') if /[a-zA-Zа-яА-Я]/.match(result[5])   # For Extrapost
+
+    #
+    # full_desc = result[1]   # For Extrastore
+    #
+
+    #
+    # if result[2].include?(' — ')
+    #   data = result[2].partition(' — ')
+    #   short_desc = data.last.split
+    #   short_desc[-2, 2] = nil
+    #   short_desc = short_desc.compact!.join(' ') + '.'
+    #   # sku_full = "lm_#{data.first.downcase.gsub(/-|[ ]/, '_')}_#{weight}"
+    #   @title = "Liqui Moly #{data.first} (#{weight} kg) (art: #{art})"
+    # elsif
+    #   result[2].include?(' - ')
+    #   data = result[2].partition(' - ')
+    #   short_desc = data.last.split
+    #   short_desc[-2, 2] = nil
+    #   short_desc = short_desc.compact!.join(' ') + '.'
+    #   @title = "Liqui Moly #{data.first} (#{weight} kg) (art: #{art})"
+    #   # sku_full = "lm_#{data.first.downcase.gsub(/-|[ ]/, '_')}_#{weight}"
+    # else
+    #   short_desc = ''
+    #   title_src = ''
+    #   data = result[2].split(' ') unless result[2].nil?
+    #   data.each do |word|
+    #     if /[А-Яа-я]/.match(word)
+    #       short_desc = short_desc + word + ' '
+    #     else
+    #       /[a-zA-Z]/.match(word)
+    #       title_src = title_src + word + ' '
+    #     end
+    #     # sku_full = "lm_#{title_src.downcase.gsub(/-|[ ]/, '_')}_#{weight}"
+    #     @title = "Liqui Moly #{title_src} (#{weight} kg) (art: #{art})"
+    #   end
+    # end
+    #
+
+
+    # create_product_extrapost(sku, purchase_price, barcode, store_id, price, short_desc, title, weight, image, filename, country_of_origin)
+    # update_product_extrapost(sku, purchase_price, barcode, store_id, price, short_desc, title, weight, image, filename, country_of_origin)
+    # update_product_extrastore(sku, price, short_desc, title, image, filename, full_desc, category_ids, availability, store_ids, old_price)
+    # create_product_extrastore(sku, price, short_desc, title, image, filename, full_desc, store_ids, category_ids, availability, old_price)
+
+    # @src_for_csv << ["#{art}", "#{title}", "#{short_desc}", "#{sku_full}", "#{barcode}", "#{purchase_price}", "#{price}", "#{weight}", "#{full_desc}"]
 
     # if sku_full.length > 32
     #     sku_full_part = sku_full.gsub(/_/, ' ').split
@@ -270,49 +366,6 @@
     # else
     #   sku_full
     # end
-
-    get_lm_product_image(product_id)
-    puts filename = @title.gsub(/ /, '_') + @content_type_data
-    image = @image
-
-    title = @title
-
-    if short_desc.include?('масло')
-      category_ids = [1206]
-    elsif short_desc.include?('салфетки')
-      category_ids = [1277]
-    else
-      category_ids = [1201]
-    end
-
-    # create_product_extrapost(sku, purchase_price, barcode, store_id, price, short_desc, title, weight, image, filename, country_of_origin)
-    # update_product_extrapost(sku, purchase_price, barcode, store_id, price, short_desc, title, weight, image, filename, country_of_origin)
-    # update_product_extrastore(sku, price, short_desc, title, image, filename, full_desc, category_ids, availability, store_ids, old_price)
-    # create_product_extrastore(sku, price, short_desc, title, image, filename, full_desc, store_ids, category_ids, availability, old_price)
-
-    # @src_for_csv << ["#{art}", "#{title}", "#{short_desc}", "#{sku_full}", "#{barcode}", "#{purchase_price}", "#{price}", "#{weight}", "#{full_desc}"]
-
-    case
-    when purchase_price.to_f <= 30
-      puts "purchase_price too small   #{art}"
-    when price.to_f <= 30
-      puts "price too small   #{art}"
-    when price.to_f < purchase_price.to_f
-      puts "purchase_price too big   #{art}"
-    when /[^0-9.,]/.match(purchase_price.to_s)
-      puts "purchase_price is NAN   #{art}"
-    when /[^0-9.,]/.match(price.to_s)
-      puts "price is NAN   #{art}"
-    when /[^0-9.,]/.match(weight.to_s)
-      puts "weight is NAN   #{art}"
-    when weight.to_f > 20
-      puts "weight > 20    #{art}"
-    when short_desc.length > 64
-      puts "short_desc length > 64    #{art}"
-    end
-
-  end
-  puts 'Number of goods:   ' + "#{art_count.size}", '- - - - -'
 
   # # To add a header, the columns should be written monotonously with the header (each data column separately)
   #
