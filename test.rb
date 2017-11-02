6bd3ef75eee3ca9194a5b21d8a6f56c9c9ec7f2e
Sproodigy <Sproodigy@gmail.com>
2017-11-02 16:59:08 +0400

    Get bardahl data

--- test.rb
+++ test.rb
@@ -15,34 +15,44 @@ require 'json'
 # page  = agent.get("https://oilbardahl.ru/avtomobili/maslo_v_dvigatel/")
 # page  = agent.get("http://bardahl-motor.ru/motornye-masla/")
 
-
-# puts array_links.each_with_index { |it, ind| puts it, ind }
 def get_bardahl_product_data
   agent = Mechanize.new
-
   page = agent.get('https://allbardahl.ru/catalog/additives/')
-
   review_links = page.links_with(text: /[a-zA-Z]/, href: %r{/\w+})[1, 76]
+
   array_links = []
-  review_links.each_with_index do |elem, i|
+  review_links.map.with_index do |elem, i|
     i % 2 == 0 ? array_links << elem : next
   end
-
-  art = array_links[4].click.search('#art-value').text
-  manufacturing = array_links[4].click.search('.item-name h2').text.split.last
-  full_desc = array_links[4].click.search('.item-text')
-  weight = array_links[4].click.search('.left span').text.rstrip
-  title = 'Bardahl ' + array_links[4].click.search('.item-name h1').text + " (#{weight})"
-  image_path = array_links[4].click.search('.item-image img').first[:src]
-  '- - - - - - - - - - - - - -'
-  { image_path: image_path}
+  array_links
 end
-# get_bardahl_product_data
+
+def get
+  get_bardahl_product_data.map do |link|
+    data = link.click
+    manufacturing = data.search('.item-name h2').text.split.last
+    next if manufacturing != 'Бельгия'
+    art = data.search('#art-value').text
+    full_desc = data.search('.item-text').to_s
+    weight = data.search('.left span').text.rstrip
+    title = 'Bardahl ' + data.search('.item-name h1').text + " (#{weight})"
+    image_path = data.search('.item-image img').first[:src]
+    store_id = 3
+    country_of_origin = 'BE'
+    # barcode = barcode_from_product_art
+    # sku = barcode
+    data = {manufacturing: manufacturing,
+            art: art
+           }
+  end
+end
+
+puts get
+
 
 def get_bardahl_product_image
   product_data = get_bardahl_product_data
   src = 'https://allbardahl.ru' + product_data[:image_path]
-  # name = get_lm_product_data(product_id)[2]
 
   content_type_data = File.extname(src)
   content_type = 'image/' + content_type_data[1,3]
@@ -52,19 +62,14 @@ def get_bardahl_product_image
   {image: image, filename: File.basename(src).gsub(/-/, '_')}
 end
 
-get_bardahl_product_image
+def barcode_from_product_art
+  s = "32667201#{get_bardahl_product_data[:art].gsub(/[^\d]/, ' ').rstrip}"
+  "#{s}#{checkdigit(s)}"
+end
 
-# info = array_links.each do |link|
-  # data = link.click
-  # puts art = data.search('.articul strong').text
-  # puts title = data.search('.hed-card.bold').text
-# end
-# data = review_links[0].click
-# puts data.search.text
-# review_links = page.links_with(text: /\w+/, href: %r{/motornye-masla/\w+})[0, 59]
-# data_source = review_links.each do |l|
-  # data = l.click
-  # pp art = data.search('.articul strong').text
-  # artist = review_meta.search('h1')[0].text
-# end
-# agent.get('http://bardahl-motor.ru/motornye-masla/xtc-5w30-20-l').search("strong").first.content
+def checkdigit(barcode)
+  evens, odds = *barcode.scan(/\d/).map { |d| d.to_i }.partition.with_index { |d, i| (i&1).zero? }
+  (10 - ((odds.reduce(:+)) * 3 + evens.reduce(:+)) % 10) % 10
+end
+
+# array_links.each_with_index { |el, i| puts el, i }
