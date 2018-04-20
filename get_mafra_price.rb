6d4026de698cf661a09a7b2d7c34dae13f1b85c5
Sproodigy <Sproodigy@gmail.com>
2018-04-20 18:12:17 +0400

    There is progress

--- get_mafra_price.rb
+++ get_mafra_price.rb
@@ -17,52 +17,91 @@ require 'active_support/core_ext/string/access'
 def get_mafra_product_data(page)
   ctx = OpenSSL::SSL::SSLContext.new
   ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  page_data = Nokogiri::HTML(HTTP.follow.get("http://www.mafrarussia.ru/?page_id=#{page}", :ssl_context => ctx).to_s)
+  page_data = Nokogiri::HTML(HTTP.get("http://www.mafrarussia.ru/?page_id=#{page}", :ssl_context => ctx).to_s)
+
+  {
+    page_response: page_data.css('h1').text,
+    title: page_data.css('.headline strong').first&.content,
+    desc1: page_data.css('.two-thirds h1').last.to_s + "\n<p></p>\n" +
+           page_data.css('.two-thirds p')[1].to_s + "\n<p></p>\n" +
+           page_data.css('.two-thirds h3')[0].to_s + "\n<p></p>\n" +
+           page_data.css('.two-thirds p')[2].to_s + "\n<p></p>\n" +
+           page_data.css('.two-thirds h3')[1].to_s + "\n<p></p>\n" +
+           page_data.css('.two-thirds p')[3].to_s,
+    desc2: page_data.css('.two-thirds .half-page h1').first.to_s + "\n<p></p>\n" +
+           page_data.css('.two-thirds .half-page p')[0].to_s + "\n<p></p>\n" +
+           page_data.css('.two-thirds .half-page h3')[0].to_s + "\n<p></p>\n" +
+           page_data.css('.two-thirds .half-page p')[1].to_s + "\n<p></p>\n" +
+           page_data.css('.two-thirds .half-page h3')[1].to_s + "\n<p></p>\n" +
+           page_data.css('.two-thirds .half-page p')[2].to_s,
+    sku_and_weight_data1: page_data.css('.two-thirds .one-third').to_a,
+    sku_and_weight_data2: page_data.css('.two-thirds .half-page p').last&.content
+  }
 end
 
-  # {
-  #   title: page.css('.headline strong').first&.content,
-  #   desc: page.css('.two-thirds h1').last.to_s + "\n" + page.css('.two-thirds p')[1].to_s + "\n" +
-  #         page.css('.two-thirds h3').first.to_s + "\n" + page.css('.two-thirds p')[2].to_s,
-  #   sku: page.css('.two-thirds strong')[1..-1].to_a,
-  #
-  #   weight1: ((page.css('.half-page p').to_s.split('Упаковка')[-1].split('<')[0].lstrip) unless (page.css('.half-page').first&.content.nil?)),
-  #   weight2: (page.css('.one-third')[0..-1].to_a unless
-  #             page.css('.one-third').first&.content.nil?)
-  # }
-# end
+start = Time.now
+goods = []
+(4027..4027).each do |page_id|
+# (3813..3818).each do |page_id|
+  source = get_mafra_product_data(page_id)
 
-def get_sku_and_weight(page)
-  source = get_mafra_product_data(page)
-
-  sku = source.css('.one-third').text
-end
-
-(3736..3736).each do |page|
-# (3814..3814).each do |page|
-source = get_mafra_product_data(page)
-
-puts sku = source.css('.two-thirds .one-third').text.split('КУПИТЬ СЕЙЧАС')
-  # res = get_mafra_product_data(page)
-  #
-  # res[:sku].each do |article|
-  #   res[]
-  #     next if article.nil?
-  #     art = article.to_s.split('>')[1].split('<')[0]
-  #     title = res[:title]
-  #
-  #     # puts res[:weight1], '- - - - - - -'
-  #     # puts res[:weight2]
-  #
-  #     if res[:weight1].nil?
-  #       weight = res[:weight2].split('Упаковка')[0]
-  #       # weight = weight[0]
-  #     else
-  #       weight = res[:weight1].split('<br>').shift
-  #     end
-  #
-  #     # weight = get_weight(art)
-  #
-      # puts sku, weight, '= = = = ='
-    # end
+  if source[:page_response] == '404'
+    puts "Page:   #{page_id} not exist"
+  else
+    puts "Page:   #{page_id}"
   end
+
+  if source[:sku_and_weight_data1].nil? || source[:sku_and_weight_data1].length == 0
+
+      sku = source[:sku_and_weight_data2].split('Упаковка')[0].split(' ')[1]
+      goods.push(sku)
+      weight = source[:sku_and_weight_data2].split('Упаковка')[1].partition('.')[0].lstrip.gsub(/,/, '.').gsub(/кг/, 'kg')
+      title = "#{source[:title]} (art: #{sku}) (#{(weight)})"
+      desc = source[:desc2]
+
+    # For Extrastore
+      # price = (purch_price * 1.34).round
+      # old_price = (purch_price * 1.49).round
+      country_of_origin = 'IT'
+      store_ids = [100]   # AvtoRaketa in Extrastore
+      availability = 'on_demand'
+      yandex_market_export = true
+   # # # # #
+      puts "Title:   #{title}", "SKU:   #{sku}", "Weight:   #{weight}", "Description:   #{desc}", '= = = = ='
+  else
+
+    source[:sku_and_weight_data1].each do |data|
+      sku = data.text.split('Упаковка')[0].split(' ')[1]
+      goods.push(sku)
+      weight = data.text.split('Упаковка')[1].partition('.')[0].lstrip.gsub(/,/, '.').gsub(/кг/, 'kg')
+      title = "#{source[:title]} (art: #{sku}) (#{(weight)})"
+      desc = source[:desc1]
+
+    # For Extrastore
+      # price = (purch_price * 1.34).round
+      # old_price = (purch_price * 1.49).round
+      country_of_origin = 'IT'
+      store_ids = [100]   # AvtoRaketa in Extrastore
+      availability = 'on_demand'
+      yandex_market_export = true
+    # # # # #
+      puts "Title:   #{title}", "SKU:   #{sku}", "Weight:   #{weight}", "Description:   #{desc}", '= = = = ='
+    end
+  end
+end
+
+puts 'Number of goods:   ' + goods.length.to_s
+finish = Time.now
+full_time = (finish - start)
+if full_time >= 3600
+  hours = (full_time / 3600).floor
+  min = (full_time / 60 - hours * 60).floor
+  sec = (full_time - (min * 60 + hours * 3600))
+  puts "#{hours} hours   #{min} min   #{sec} sec"
+elsif full_time >= 60
+  min = (full_time / 60).floor
+  sec = (full_time - min * 60).floor
+  puts "#{min} min   #{sec} sec"
+else
+  puts full_time.round.to_s + ' sec'
+end
